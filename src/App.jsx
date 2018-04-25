49a62d609a16cdfe8119a70a3104ac31b0ee40ad
COM <>
2018-04-25 15:35:09 +0900

    Search city and get coordination
    Search city and get coordination in src/utils/cityCoord.js.
    For searching faster, make src/utils/firstCities.js which has first
    cities like Seoul, Incheon, etc.

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { Provider } from 'react-redux';
 import './App.css';
-import CityWeatherList from './component/CityWeatherList'
+import CityWeatherList from './component/CityWeatherList';
 import store from './store';
-import Constants from './utils/Constants'
+import Constants from './utils/Constants';
+import cityCoordList from './utils/cityCoord';
+import firstCities from './utils/firstCities';
 
 import fetch from 'isomorphic-fetch';
 
@@ -11,6 +13,52 @@ class App extends Component {
 
   constructor(props) {
     super(props);
+
+    let isFirst = false;
+    let firstCity = '';
+    const testUserInput1 = '서울';
+    const testUserInput2 = '동작';
+    const testUserInput3 = '송도';
+
+    let userInput = testUserInput3;
+    
+    for(let i=0 ; i<firstCities.length ; i++) {
+      if(firstCities[i].includes(testUserInput1)) {
+        isFirst = true;
+        firstCity = firstCities[i];
+        break;
+      }
+    }
+    
+    let city = '';
+
+    if(isFirst) {
+      city = cityCoordList.find((city) => {
+        return city.first === firstCity;
+      })
+    }
+
+    let first = isFirst ? firstCity : '';
+    let second = '';
+    let third = '';
+
+    for(let i=0 ; i<cityCoordList.length; i++) {
+      city = cityCoordList[i];
+
+      second = String(city.second); 
+      third = String(city.third);
+      
+      if(second.includes(userInput)) {
+        coordX = city.X;
+        coordY = city.Y;
+        break;
+      }
+      else if(third.includes(userInput)) {
+        coordX = city.X;
+        coordY = city.Y;
+        break;
+      }
+    }
 
     //현재 날짜 시간
     let dateStr = new Date().toLocaleDateString();
@@ -22,26 +70,27 @@ class App extends Component {
 
     // 초단기실황 ( 매시간 30분에 생성되고 10분마다 동일한 파일로 최신 정보를 업데이트 됨)
     let hour = time.getHours();
-    hour = time.getMinutes() <= 40 ? (hour-1).toString() : hour.toString();
+    hour = time.getMinutes() <= 30 ? (hour-1).toString() : hour.toString();
     let minute = '00';
     let currentDate = year + month + day;
     let currentTime = hour + minute;
     console.log(currentTime);
     
-    const { SERVICE_KEY, 
-            URL, 
-            PAGE_NUMBER, 
-            NUMBER_OF_ROWS, 
-            TYPE, 
-            TEST_X, 
-            TEST_Y 
+    const { 
+      SERVICE_KEY, 
+      URL, 
+      PAGE_NUMBER, 
+      NUMBER_OF_ROWS, 
+      TYPE 
+      // TEST_X, 
+      // TEST_Y 
           } = Constants;
       
     const serviceKey = SERVICE_KEY;
     const baseDate = currentDate;
     const baseTime = currentTime;
-    const coordX = TEST_X;
-    const coordY = TEST_Y;
+    let coordX = city.X;
+    let coordY = city.Y;
     const pageNo = PAGE_NUMBER;
     const numOfRows = NUMBER_OF_ROWS;
     const type = TYPE;
@@ -58,7 +107,7 @@ class App extends Component {
               ;
    
     let currentTemperature = '';          
-    fetch(url)
+    fetch(url) // 0)
     .then(res => res.json())
     .then((data) => {
       // 2) 그다음 
